ba740983215e4334d4a89e58383856adbd115f6b
gk811 <gptz1811@gmail.com>
2025-09-20 01:02:10 +0530

    ch2

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,57 +1,57 @@
-// import { createContext, useContext, useState, useEffect } from 'react';
-// import api from '../services/api';
+import { createContext, useContext, useState, useEffect } from 'react';
+import api from '../services/api';
 
-// const AuthContext = createContext();
+const AuthContext = createContext();
 
-// export const AuthProvider = ({ children }) => {
-//   const [user, setUser] = useState(null);
-//   const [loading, setLoading] = useState(true);
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-//   useEffect(() => {
-//     const checkAuth = async () => {
-//       try {
-//         const res = await api.get('/user/me');
-//         setUser(res.data.user);
-//       } catch (err) {
-//         setUser(null);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-//     checkAuth();
-//   }, []);
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await api.get('/user/me');
+        setUser(res.data.user);
+      } catch (err) {
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    checkAuth();
+  }, []);
 
-//   const signup = async (name, email, password) => {
-//     await api.post('/auth/signup', { name, email, password });
-//   };
+  const signup = async (name, email, password) => {
+    await api.post('/auth/signup', { name, email, password });
+  };
 
-//   const login = async (email, password) => {
-//     await api.post('/auth/login', { email, password });
-//     const res = await api.get('/user/me');
-//     setUser(res.data.user);
-//   };
+  const login = async (email, password) => {
+    await api.post('/auth/login', { email, password });
+    const res = await api.get('/user/me');
+    setUser(res.data.user);
+  };
 
-//  const logout = async () => {
-//   try {
-//     await api.post('/auth/logout');
-//     setUser(null);
-//   } catch (err) {
-//     console.error('Logout failed:', err);
-//   }
-// };
+ const logout = async () => {
+  try {
+    await api.post('/auth/logout');
+    setUser(null);
+  } catch (err) {
+    console.error('Logout failed:', err);
+  }
+};
 
-// const forgotPassword = async (email) => {
-//     return api.post('/auth/forgot-password', { email });
-//   };
+const forgotPassword = async (email) => {
+    return api.post('/auth/forgot-password', { email });
+  };
 
-//   return (
-//     <AuthContext.Provider value={{ user, loading, signup, login, logout, forgotPassword }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
+  return (
+    <AuthContext.Provider value={{ user, loading, signup, login, logout, forgotPassword }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
-// export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => useContext(AuthContext);
 
 
 
